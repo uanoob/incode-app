31045715d8f338d9ebcb6698ff1286e7f155ed29
uanoob <usnet@ukr.net>
2018-10-07 15:11:39 +0300

    Updates

--- precache-manifest.aa2f3372c18da6ac760cee574f0e3a6e.js
+++ precache-manifest.aa2f3372c18da6ac760cee574f0e3a6e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/incode-app/static/js/runtime~main.2cfb26dc.js"
   },
   {
-    "revision": "5219c7fd8d19d64322b6",
-    "url": "/incode-app/static/js/main.5219c7fd.chunk.js"
+    "revision": "a1fc34566a24d9fafc82",
+    "url": "/incode-app/static/js/main.a1fc3456.chunk.js"
   },
   {
     "revision": "b72372c81390e2f21257",
     "url": "/incode-app/static/js/1.b72372c8.chunk.js"
   },
   {
-    "revision": "5219c7fd8d19d64322b6",
+    "revision": "a1fc34566a24d9fafc82",
     "url": "/incode-app/static/css/main.47e4611d.chunk.css"
   },
   {
-    "revision": "f7b98ed22014faf12b454869bafb7e96",
+    "revision": "cfaaeb313ff07d8b67e8a50b86f8ed1d",
     "url": "/incode-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/incode-app/precache-manifest.a9f7c38ecf8e3d3aaae4e0e3694c48f7.js"
+  "/incode-app/precache-manifest.aa2f3372c18da6ac760cee574f0e3a6e.js"
 );
 
 workbox.clientsClaim();
